77b8165a56fe6c53c16a58748ffcd42f4691ef5e
Paalar <paaledward@gmail.com>
2020-09-12 20:48:29 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        browser: true,
+        es2020: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            tsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'default-case': 'error',
+        'default-case-last': 'error',
+        curly: ['error', 'multi'],
+        'no-shadow': 'error',
+        'block-spacing': 'error',
+        'comma-dangle': ['error', {
+            arrays: 'never',
+            objects: 'never',
+            imports: 'never'
+        }],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'key-spacing': ['error', { beforeColon: false }],
+        'max-len': ['error', { code: 110, ignoreUrls: true }],
+        'comma-spacing': ['error', { before: false, after: true }],
+        'no-multiple-empty-lines': 'error',
+        'no-trailing-spaces': 'error',
+        'quote-props': ['error', 'as-needed']
+    }
+};
